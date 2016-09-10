bd6f9534da7d5c6cfa21776cb42b180e4a55a206
Ryan Newton <rnewton@hearsaycorp.com>
2016-09-10 13:31:04 -0700

    Create basic layout for cameraview

---
+++ app/src/main/java/com/fadetoproductions/rvkn/clothesconsensus/activity/CameraActivity.java
@@ -0,0 +1,15 @@
+package com.fadetoproductions.rvkn.clothesconsensus.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.fadetoproductions.rvkn.clothesconsensus.R;
+
+public class CameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+    }
+}
