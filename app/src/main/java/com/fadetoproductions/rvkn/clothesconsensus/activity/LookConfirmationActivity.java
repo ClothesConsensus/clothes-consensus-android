fb32219895d7ed76ba7bb8b57be5445d52d3d6d1
Ryan Newton <rnewton@hearsaycorp.com>
2016-09-11 16:53:32 -0700

    Get look posting aligned with backend again

--- app/src/main/java/com/fadetoproductions/rvkn/clothesconsensus/activity/LookConfirmationActivity.java
+++ app/src/main/java/com/fadetoproductions/rvkn/clothesconsensus/activity/LookConfirmationActivity.java
@@ -6,12 +6,19 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.fadetoproductions.rvkn.clothesconsensus.R;
+import com.fadetoproductions.rvkn.clothesconsensus.models.User;
 import com.fadetoproductions.rvkn.clothesconsensus.utils.PhotoUtils;
 
 import org.json.JSONObject;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -20,14 +27,18 @@ import butterknife.OnClick;
 public class LookConfirmationActivity extends BaseActivity {
 
     Bitmap lookImage;
+    User user;
+
     @BindView(R.id.ivLookImage) ImageView ivLookImage;
     @BindView(R.id.btnPostLook) Button btnPostLook;
+    @BindView(R.id.etMessage) EditText etMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_look_confirmation);
         ButterKnife.bind(this);
+        user = User.getLoggedInUser(this);
         loadLookImage();
     }
 
@@ -40,7 +51,17 @@ public class LookConfirmationActivity extends BaseActivity {
     @OnClick(R.id.btnPostLook)
     public void postLook(View view) {
         // Some post logic here
-        client.postLook(lookImage);
+
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'");
+        TimeZone tz = TimeZone.getTimeZone("UTC");
+        df.setTimeZone(tz);
+        String expirationString = df.format(new Date());
+
+        String message = etMessage.getText().toString();
+        String imageString = PhotoUtils.encodeBitmapToSendableString(lookImage);
+
+
+        client.postLook(user.getUserId(), message, expirationString, imageString);
     }
 
     @Override
