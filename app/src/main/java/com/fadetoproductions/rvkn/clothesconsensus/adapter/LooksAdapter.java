bfa515e90ebd582eeb87062bbfff25cdbcf9da57
Shashank Dass <sdass@hearsaycorp.com>
2016-09-17 16:21:04 -0700

    Adding cards and bounce animation on scroll of home activity.

--- app/src/main/java/com/fadetoproductions/rvkn/clothesconsensus/adapter/LooksAdapter.java
+++ app/src/main/java/com/fadetoproductions/rvkn/clothesconsensus/adapter/LooksAdapter.java
@@ -10,6 +10,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.EditText;
 import android.widget.ImageView;
 
@@ -42,7 +44,10 @@ public class LooksAdapter extends RecyclerView.Adapter<LooksAdapter.LookViewHold
         this.mContext = mContext;
         this.mLooks = looks;
     }
-
+    public void animate(RecyclerView.ViewHolder viewHolder) {
+        final Animation animAnticipateOvershoot = AnimationUtils.loadAnimation(mContext, R.anim.bounce);
+        viewHolder.itemView.setAnimation(animAnticipateOvershoot);
+    }
     public interface LookVoteListener {
         void onVoteLook(Look look, Boolean vote);
     }
@@ -58,7 +63,7 @@ public class LooksAdapter extends RecyclerView.Adapter<LooksAdapter.LookViewHold
     @Override
     public void onBindViewHolder(LooksAdapter.LookViewHolder viewHolder, int position) {
         Look look = mLooks.get(position);
-
+        animate(viewHolder);
         LookViewHolder lvh = viewHolder;
         if (look != null) {
             lvh.bindLook(look);
