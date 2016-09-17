d23eb216784238c137954ec4b45d52fa031a620a
Ryan Newton <rnewton@hearsaycorp.com>
2016-09-17 09:58:31 -0700

    Allow you to click on any user

--- app/src/main/java/com/fadetoproductions/rvkn/clothesconsensus/adapter/LooksAdapter.java
+++ app/src/main/java/com/fadetoproductions/rvkn/clothesconsensus/adapter/LooksAdapter.java
@@ -4,16 +4,17 @@ package com.fadetoproductions.rvkn.clothesconsensus.adapter;
  * Created by sdass on 8/31/16.
  */
 
-import android.content.Context;
 import android.databinding.DataBindingUtil;
 import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.fadetoproductions.rvkn.clothesconsensus.R;
+import com.fadetoproductions.rvkn.clothesconsensus.activity.BaseActivity;
 import com.fadetoproductions.rvkn.clothesconsensus.databinding.LookCardBinding;
 import com.fadetoproductions.rvkn.clothesconsensus.models.Look;
 import com.fadetoproductions.rvkn.clothesconsensus.utils.ItemTouchHelperAdapter;
@@ -27,17 +28,17 @@ import jp.wasabeef.picasso.transformations.RoundedCornersTransformation;
 
 public class LooksAdapter extends RecyclerView.Adapter<LooksAdapter.LookViewHolder> implements ItemTouchHelperAdapter {
 
-    private Context mContext;
+    private BaseActivity mContext;
 
     private List<Look> mLooks;
 
-    private Context getContext() {
+    private BaseActivity getContext() {
         return mContext;
     }
 
     public LookVoteListener lookVoteListener;
 
-    public LooksAdapter(Context mContext, List<Look> looks) {
+    public LooksAdapter(BaseActivity mContext, List<Look> looks) {
         this.mContext = mContext;
         this.mLooks = looks;
     }
@@ -97,11 +98,11 @@ public class LooksAdapter extends RecyclerView.Adapter<LooksAdapter.LookViewHold
         // Your holder should contain a member variable
         // for any view that will be set as you render a row
         final LookCardBinding lookCardBinding;
-        private Context mContext;
+        private BaseActivity mContext;
         Look look;
         // We also create a constructor that accepts the entire item row
         // and does the view lookups to find each subview
-        public LookViewHolder(Context context, LookCardBinding binding) {
+        public LookViewHolder(BaseActivity context, LookCardBinding binding) {
             // Stores the itemView in a public final member variable that can be used
             // to access the context from any ViewHolder instance.
             super(binding.getRoot());
@@ -109,44 +110,24 @@ public class LooksAdapter extends RecyclerView.Adapter<LooksAdapter.LookViewHold
             this.mContext = context;
         }
 
-
-//        // We also create a constructor that accepts the entire item row
-//        // and does the view lookups to find each subview
-//        public LookViewHolder(Context context, LookCardBinding binding, FragmentManager fragmentManager) {
-//            // Stores the itemView in a public final member variable that can be used
-//            // to access the context from any ViewHolder instance.
-//            super(binding.getRoot());
-//            this.lookCardBinding = binding;
-//            this.mContext = context;
-//            this.mFm = fragmentManager;
-//
-//        }
         public void bindLook(final Look look){
             this.look = look;
             ImageView thumbnail = lookCardBinding.rlCaption.ivThumbnail;
             EditText message = lookCardBinding.rlCaption.etMessage;
             message.setText(look.getMessage());
             ImageView ivLook = lookCardBinding.ivLookImage;
-//            RatingBar rb = lookCardBinding.rbVotes;
-//            ImageView ivTimer = lookCardBinding.ivTimer;
-//            TextView tvTime = lookCardBinding.tvTime;
             thumbnail.setImageResource(0);
             ivLook.setImageResource(0);
-//            ivTimer.setImageResource(0);
             Picasso.with(mContext).load(look.getUser().getProfileImageUrl()).transform(new RoundedCornersTransformation(5,0)).fit().into(thumbnail);
             Picasso.with(mContext).load(look.getPhotoUrl()).transform(new RoundedCornersTransformation(10,0, RoundedCornersTransformation.CornerType.ALL)).fit().into(ivLook);
-//            ivTimer.setImageResource(R.drawable.ic_access_time_black_24dp);
-//            ivTimer.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View view) {
-//                    TimePickerFragment newFragment = new TimePickerFragment();
-//                    newFragment.show(mFm, "timePicker");
-//                }
-//            });
-//            message.setText(look.getMessage());
-//            rb.setRating(look.findAverageRating());
-//            tvTime.setText(look.getHour()+":"+look.getMinute());
 
+            // TODO This shouldn't be done this way
+            thumbnail.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    mContext.loadProfileForUser(look.getUser());
+                }
+            });
         }
 
         @Override
@@ -157,7 +138,6 @@ public class LooksAdapter extends RecyclerView.Adapter<LooksAdapter.LookViewHold
         @Override
         public void onItemClear() {
             itemView.setBackgroundColor(0);
-
         }
     }
 
